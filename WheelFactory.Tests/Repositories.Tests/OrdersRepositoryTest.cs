404fbe1f9b9aa45623a0993a790649104995c94c
Sathvik Reddy Kolla <ksathvikreddy@actalentservices.com>
2024-10-15 14:18:30 +0530

    added test project

---
+++ WheelFactory.Tests/Repositories.Tests/OrdersRepositoryTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WheelFactory.Tests.Repositories.Tests
+{
+    internal class OrdersRepositoryTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
